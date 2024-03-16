6c0d134780e5d687680267bcb5a3218fad422ad5
MariskaPutri18 <mariskaputri960@gmail.com>
2024-03-16 19:58:10 +0700

    menu

--- mainPangkat18.java
+++ mainPangkat18.java
@@ -9,23 +9,46 @@ public class mainPangkat18 {
         pangkat18 [] png = new pangkat18[elemen];
 
         for (int i = 0; i < elemen; i++){
-            png[i] = new pangkat18();
-            System.out.print("Masukkan nilai yang akan dipangkatkan ke-"+(i+1)+" : ");
-            png[i].nilai = sc.nextInt();
-            System.out.print("Masukkan nilai pemangkat ke-" +(i+1)+ " : ");
-            png[i].pangkat = sc.nextInt();
+            System.out.print("Masukkan nilai yang akan dipangkatkan ke-" + (i + 1) + " : ");
+            int nilai = sc.nextInt();
+            System.out.print("Masukkan nilai pemangkat ke-" + (i + 1) + " : ");
+            int pangkat = sc.nextInt();
+            png[i] = new pangkat18(nilai, pangkat);
         }
-
-        System.out.println("==========================================");
-        System.out.println("Hasil Pangkat dengan Brute Force");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Nilai "+ png[i].nilai+" pangkat " +png[i].pangkat +" adalah "+ png[i].pangkatBF(png[i].nilai,png[i].pangkat));
+        boolean ulang = true;
+        while (ulang) {
+            
+        
+        System.out.println("======ALGORITMA UNTUK MENGHITUNG=====");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide and Conquer");
+        System.out.println("3. Keluar");
+        System.out.println("=====================================");
+        System.out.println("Masukkan Pilihan : ");
+        int Pilihan = sc.nextInt();
+        switch (Pilihan) {
+            case 1:
+                System.out.println("==========================================");
+                System.out.println("Hasil Pangkat dengan Brute Force");
+                    for (int i = 0; i < elemen; i++) {
+                     System.out.println("Nilai "+ png[i].nilai+" pangkat " +png[i].pangkat +" adalah "+ png[i].pangkatBF(png[i].nilai,png[i].pangkat));
+                    }   
+                break;
+            case 2:
+                System.out.println("==========================================");
+                System.out.println("Hasil Pangkat dengan Divide and Conquer");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Nilai "+ png[i].nilai+" pangkat "+ png[i].pangkat +" adalah "+ png[i].pangkatDC(png[i].nilai,png[i].pangkat));
+                } 
+                break;
+            case 3:
+                 ulang = false;
+                System.out.println(" anda keluar dari program");
+                break;
+            default:
+                System.out.println("Pilihan Tidak valid");
+                break;
         }
-
-        System.out.println("==========================================");
-        System.out.println("Hasil Pangkat dengan Divide and Conquer");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Nilai "+ png[i].nilai+" pangkat "+ png[i].pangkat +" adalah "+ png[i].pangkatDC(png[i].nilai,png[i].pangkat));
-        }
+    }
     }
 }
--- pangkat18.java
+++ pangkat18.java
@@ -1,6 +1,11 @@
 public class pangkat18 {
     public int nilai, pangkat;
 
+    public pangkat18(int nilai, int pangkat){
+    this.nilai = nilai;
+    this.pangkat = pangkat;
+    }
+
     public int pangkatBF (int a, int n){
          int hasil = 1;
         for (int i = 0; i < n; i ++){
@@ -10,14 +15,14 @@ public class pangkat18 {
     }
 
     public int pangkatDC (int a, int n){
-        if (n ==0){
+        if (n ==0){  
             return 1;
         }
         else 
        {
             if (n%2==1)
                 return (pangkatDC(a, n/2)*pangkatDC(a,n/2)*a);
-            else// bilangan genap
+            else
                 return(pangkatDC(a,n/2)*pangkatDC(a,n/2));
        }
     }
