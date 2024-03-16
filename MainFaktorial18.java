04666d65adbe3fdb8a1f5b85df126534ffeec169
MariskaPutri18 <mariskaputri960@gmail.com>
2024-03-16 19:02:59 +0700

    waktu eksekusi

--- MainFaktorial18.java
+++ MainFaktorial18.java
@@ -16,12 +16,20 @@ public class MainFaktorial18 {
         System.out.println("================================================");
         System.out.println("Hasil Faktorial dengan Brute Force");
         for (int i = 0; i < elemen; i++){
-            System.out.println("Faktorial dari nilai" + fk[i].nilai + "adalah : "  + fk[i].faktorialBF(fk[i].nilai));
+            long startTime = System.currentTimeMillis();
+            System.out.println("Faktorial dari nilai " + fk[i].nilai + " adalah : " + fk[i].faktorialBF(fk[i].nilai));
+            long endTime = System.currentTimeMillis();
+            long executionTime = endTime - startTime;
+            System.out.println("Waktu eksekusi: " + executionTime + " ms");
         }
         System.out.println("================================================");
         System.out.println("Hasil Faktorial dengan Divide and Conquer");
         for (int i = 0; i < elemen; i++){
-            System.out.println("Faktorial dari nilai"  + fk[i].nilai + "adalah : "  + fk[i].faktorialDC(fk[i].nilai));
+            long startTime = System.currentTimeMillis();
+            System.out.println("Faktorial dari nilai " + fk[i].nilai + " adalah : " + fk[i].faktorialDC(fk[i].nilai));
+            long endTime = System.currentTimeMillis();
+            long executionTime = endTime - startTime;
+            System.out.println("Waktu eksekusi: " + executionTime + " ms");
         }
         System.out.println("===================================================");
 
