599e9d931a63c04103a4c64f238e60bdbf7b56dc
MariskaPutri18 <mariskaputri960@gmail.com>
2024-03-16 11:42:19 +0700

    ubah for

--- Faktorial18.java
+++ Faktorial18.java
@@ -1,12 +1,12 @@
 public class Faktorial18 {
     public int nilai;
 
-    public int faktorialBF (int n){
-        int fakto = 1;
-        for (int i = 1; i <= n; i ++){
-            fakto = fakto * i;
+    public int faktorialBF(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * faktorialBF(n - 1);
         }
-        return fakto;
     }
     public int faktorialDC (int n){
         if (n ==1){
